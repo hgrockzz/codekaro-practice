cb41c1ec74c8e44b8d1402280bc363392af4c9c5
Harshit Gupta <87670425+hgrockzz@users.noreply.github.com>
2023-07-10 00:47:02 +0530

    movies ratings, trailer update

--- practice/random/p5_movies.js
+++ practice/random/p5_movies.js
@@ -1,20 +1,34 @@
 let api = 'http://www.omdbapi.com/?apikey=cf2dfa76&t='
 
+//find movies on pressing "enter"
+document.getElementById("search").addEventListener("keypress", function(event) {
+    if (event.key === "Enter"){
+        event.preventDefault();
+        document.getElementById('btn').click();
+    }   
+});
 
 function findMovie(){
     let movie = document.getElementById('search');
+    // let trailer = document.createElement("a");
+    document.getElementById('trailer-box').innerText = "";
+
     fetch(api + movie.value).then(function (response) {
         return response.json()
     }).then(function (data) {
         document.getElementById('landing').classList.add('gayab');
         document.getElementById('movie-info').classList.remove('gayab');
-        document.getElementById('title').innerText = data.Title;
+        document.getElementById('title').innerText = data.Title + " (" + data.Year + ")";
         document.getElementById('desc').innerText = data.Plot;
-        document.getElementById('rating').innerText = data.Ratings[0].Value;
-        document.getElementById('year').innerText = data.Year;
         document.getElementById('director').innerText = data.Director;
         document.getElementById('genre').innerText = data.Genre;
+        document.getElementById('imdb').innerText = data.imdbRating + " ⭐";
         document.getElementById('poster').src = data.Poster;
+        let trailer = document.createElement("a");
+        trailer.innerText = "Watch Trailer";
+        trailer.href = "https://www.youtube.com/results?search_query=" + data.Title +" trailer";
+        trailer.classList.add("trailer","btn");
+        document.getElementById('trailer-box').appendChild(trailer);
         console.log(data);
     })
 }
